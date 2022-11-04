f6d6eda1be6d302926c7f54cd648e8b8be3e0f11
Robert <robert.schlegel@imev-mer.fr>
2022-11-04 18:49:30 +0100

    First exercise

---
+++ course_material/exercises/plots_1_basics.R
@@ -0,0 +1,30 @@
+# Script name
+# Author
+# Date
+
+
+# Libraries ---------------------------------------------------------------
+
+library(tidyverse)
+library(palmerpenguins)
+
+
+# Data --------------------------------------------------------------------
+
+# Load the dataset into the local environment
+penguins <- penguins
+
+
+# Analyses ----------------------------------------------------------------
+
+# Nothing for now
+
+
+# Plots -------------------------------------------------------------------
+
+# 1: Create a basic plot with different x and y axes
+
+# 2: Change the aes() arguments
+
+# 3: Change the labels
+
