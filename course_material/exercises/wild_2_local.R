64b9f3518815d6c998124dc904bf57c53f338915
robwschlegel <robwschlegel@gmail.com>
2022-11-23 00:31:14 +0100

    Finished wild2 slides and exercises

--- course_material/exercises/wild_2_local.R
+++ course_material/exercises/wild_2_local.R
@@ -26,15 +26,24 @@
 
 # Exercise 2 --------------------------------------------------------------
 
-#
+# Download a NetCDF file with multiple dates
+# Extract three bounding boxes from all dates
+# Calculate the grouped mean per date per bounding box
+# Inset each time series over a map of that bounding box
+  # showing the day with the highest value being plotted
 
 
 # Exercise 3 --------------------------------------------------------------
 
-#
+# Write a script that performs a full workflow with one click:
+  # Load libraries
+  # Download data
+  # Manipulate into tidy data
+  # Perform analyses
+  # Create and save publication quality plot
 
 
 # BONUS -------------------------------------------------------------------
 
-#
+# Smile and enjoy life :)
 
