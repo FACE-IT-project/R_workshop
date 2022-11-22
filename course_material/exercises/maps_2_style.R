1de4d1eb4cae2e47a3d9897db20c6fa7a3285130
robwschlegel <robwschlegel@gmail.com>
2022-11-22 19:09:06 +0100

    Updates to tidy exercises

--- course_material/exercises/maps_2_style.R
+++ course_material/exercises/maps_2_style.R
@@ -5,10 +5,12 @@
 
 # Libraries ---------------------------------------------------------------
 
+library(tidyverse)
+
 # The two packages require for 'marmap' to work
 library(rgdal)
 library(raster)
-
+library(ggOceanMaps)
 
 # Data --------------------------------------------------------------------
 
@@ -29,10 +31,36 @@ ggplot() +
 # Download bathymetry data and plot them
 
 
+basemap(limits = c(-160, -80, 65, 90), rotate = TRUE, bathymetry = TRUE)
+
+
 # Exercise 2 --------------------------------------------------------------
 
 # Chose a different region and get bathymetry
 # Plot and combine the two figures
+
+map_global_fix %>% 
+  filter(lon > 9, lon < 28, lat > 76, lat < 81) %>% 
+  ggplot(aes(x = lon, y = lat)) +
+  geom_polygon(aes(group = group)) +
+  # Filtering the OISST_2022 data directly in geom_tile()
+  geom_tile(data = filter(OISST_2022,
+                          lon > 9, lon < 28, lat > 76, lat < 81), 
+            aes(fill = temp)) +
+  coord_map(projection = "ortho", orientation = c(90, 0, 0))
+
+# Map 1
+map_1 <- basemap(limits = 60)
+map_2 <- basemap(limits = c(-160, -80, 60, 85), rotate = TRUE)
+
+ggdraw() +
+  draw_plot(map_1) +
+  draw_plot(map_2, x = 0.05, y = 0.65, width = 0.3, height = 0.3)
+
+# Create dataframe to add points
+df_points <- data.frame(lon = c(10, 12),
+                        lat = c(34, 36),
+                        point_colour = c("red", "blue"))
 
 
 # Exercise 3 --------------------------------------------------------------
